7a9f850f238f7663ab2fcb97db3ccf2cb5bceb48
OlivierBelaud <olivierbelaudpro@gmail.com>
2020-09-07 18:42:18 +0100

    chore: add nodemon

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 const PORT = 8000;
-app.get('/', (req,res) => res.send('Docker Vue Heroku'));
+app.get('/', (req,res) => res.send('Docker Vue Heroku Hello World !'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
