4a0b1d4cb8301c9cf5b9fb8ea364af3706f645e1
OlivierBelaud <olivierbelaudpro@gmail.com>
2020-09-07 18:22:04 +0100

    fix: http vs https for localhost

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express();
 const PORT = 8000;
 app.get('/', (req,res) => res.send('Docker Vue Heroku'));
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
