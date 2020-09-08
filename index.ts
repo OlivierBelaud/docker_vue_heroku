326090611ec60f4747778dce4816add69c26c351
OlivierBelaud <olivierbelaudpro@gmail.com>
2020-09-08 15:14:15 +0100

    chore: add docker

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 
 const app = express();
-const PORT = 8000;
-app.get('/', (req,res) => res.send('Docker Vue Heroku Hello World !'));
+const PORT = 8080;
+app.get('/', (req,res) => res.send('Hello World !'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
